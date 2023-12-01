413f7df60e6f2107e1b31b22b8a6dc3dade52b67
Fatih Etlik <fatihe307@gmail.com>
2023-12-02 00:08:45 +0300

    All Changes Applied .

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   port: 3030
-  // }
+  build: {
+    outDir: 'client/dist', // Set the output directory
+  },
+  base: '/',
 })
