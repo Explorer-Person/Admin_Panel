33ffa0fa4712fbbb6ecc3f485687d9cc92b196cd
Fatih Etlik <fatihe307@gmail.com>
2023-12-01 12:58:29 +0300

    All Changes Applied .

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(handleErrors);
 
 const PORT = process.env.PORT || 3005;
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server Connection Provided by PORT: ${PORT}`);
 });
 
