3252d45c969bdfd293f7552f846897c953b1a31d
Fatih Etlik <fatihe307@gmail.com>
2023-12-02 00:21:58 +0300

    All Changes Applied .

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,18 +4,4 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: [
-        '.js',
-        '.json',
-        '.jsx',
-        '.mjs',
-        '.ts',
-        '.tsx',
-        '.vue',
-    ],
-},
-server: {
-    cors: true,
-},
 })
